d7788d8645f1b5909f38398a39ee87c3ae03d06b
Samuel E. Moelius III <sam@moeli.us>
2018-11-23 04:15:39 -0500

    Adjusting conversion test names.

--- common/test/conversion.ts
+++ common/test/conversion.ts
@@ -16,16 +16,16 @@ describe("conversions", () => {
   const stride = 32
   for (let length = 0; length <= max_length; length += stride) {
     const x = new BN(1).shln(length).subn(1)
-    it(title("x = uint256_from_ipfs_multihash(ipfs_multihash_from_uint256(x))", length), async () =>
+    it(title("x = (uint256_from_ipfs_multihash o ipfs_multihash_from_uint256)(x)", length), async () =>
       assert(x.eq(conversion.uint256_from_ipfs_multihash(conversion.ipfs_multihash_from_uint256(x))))
     )
-    it(title("x = uint256_from_buffer(buffer_from_uint256(x))", length), async () =>
+    it(title("x = (uint256_from_buffer o buffer_from_uint256)(x)", length), async () =>
       assert(x.eq(conversion.uint256_from_buffer(conversion.buffer_from_uint256(x))))
     )
-    it(title("x = bn_from_bignumber(bignumber_from_bn(x))", length), async () =>
+    it(title("x = (bn_from_bignumber o bignumber_from_bn)(x)", length), async () =>
       assert(x.eq(conversion.bn_from_bignumber(conversion.bignumber_from_bn(x))))
     )
-    it(title("x = web3.utils.toBN(to_hex(x))", length), async () =>
+    it(title("x = (web3.utils.toBN o to_hex)(x)", length), async () =>
       assert(x.eq(Web3.utils.toBN(conversion.to_hex(x))))
     )
   }
